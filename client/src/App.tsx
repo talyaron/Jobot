12f1e7c1b6b045a461f2a3440a70e7d19183cf02
Yosef Ibrahim <yosefib88@gmail.com>
2025-02-25 21:06:11 +0200

    DesignCVWizard-update-personalInformation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Auth from './view/pages/auth/Auth'
 import Candidate from './view/pages/candidate/Candidate'
+import DesignCvWizard from './view/pages/designCvWizard/DesignCvWizard'
 
 function App() {
 
@@ -10,7 +10,7 @@ function App() {
       <div>
         <h1>Jobot system</h1>
         <br />
-        <Auth></Auth>
+        <DesignCvWizard />
       </div>
   )
 }
