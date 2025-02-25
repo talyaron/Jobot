2f9b04c9800bf092fff0be8f3d6828b07a49d422
silam50 <ysilam50@gmail.com>
2025-02-25 18:37:32 +0200

    jobCandidate

---
+++ server/src/Routes/jobRoutes.ts
@@ -0,0 +1,14 @@
+import  express from "express"
+import { loginUser } from "../Controllers/user/userLoginCont";
+import { registerUser } from "../Controllers/user/usrRegCont";
+import { get } from "http";
+import { getJobDetails } from "../Controllers/job/getJobDetails";
+
+
+
+const router = express.Router()
+
+router.get(`/job-details/:id`, getJobDetails);
+
+
+export default router;
