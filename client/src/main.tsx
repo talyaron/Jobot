b763270b56e4128fb6c0e93c2d08c50b1e9ce733
Tal Yaron <tal.yaron@gmail.com>
2025-02-25 18:34:59 +0200

    fixed sum errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import FirstPage from './view/pages/firstPage/FirstPage.tsx'
 import Wizard from './view/pages/wizard/Wizard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-    <App />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<FirstPage />} />
--- client/src/view/pages/firstPage/FirstPage.tsx
+++ client/src/view/pages/firstPage/FirstPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import TextChanger from '../textChanger/TextChanger';
 import styles from './FirstPage.module.scss';
 
