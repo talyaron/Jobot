fb3a76e8e33804aa71f6084b8f2004c72931173b
Elizigi <elizigi876@gmail.com>
2025-02-25 19:27:03 +0200

    removed useless  lines

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,15 @@
-import './App.css'
-import Auth from './view/pages/auth/Auth'
-import Candidate from './view/pages/candidate/Candidate'
-import JobApplication from './view/pages/jobApplication/JobApplication'
+import "./App.css";
+import Auth from "./view/pages/auth/Auth";
+import Candidate from "./view/pages/candidate/Candidate";
 
 function App() {
-
-  if(JobApplication)
-  return (<JobApplication></JobApplication>)
-
-
   return (
-    
-
-      <div>
-        <h1>Jobot system</h1>
-        <br />
-        <Auth></Auth>
-      </div>
-  )
+    <div>
+      <h1>Jobot system</h1>
+      <br />
+      <Auth></Auth>
+    </div>
+  );
 }
 
-export default App
+export default App;
