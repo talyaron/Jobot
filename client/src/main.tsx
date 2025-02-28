d15ff47fb83b51ca2c344d9265f866d4f05c95b8
Cherno <daniel.cherno9@gmail.com>
2025-02-28 13:17:20 +0200

    fixed merging errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,10 +8,9 @@ import Login from './view/pages/login/Login.tsx'
 import Home from './view/pages/home/Home.tsx'
 import JobCandidate from './view/pages/jobCandidate/JobCandidate.tsx'
 import EmployerWrapper from './view/pages/employerWrapper/EmployerWrapper.tsx'
-import ApplicationStatus from './view/pages/applicationstatus/ApplicationStatus.tsx'
 import JobsEmployer from './view/pages/jobsEmployer/JobsEmployer.tsx'
-import JobApplication from './view/pages/jobApplication/JobApplication.tsx'
 import Chat from './view/pages/chat/Chat.tsx'
+import JobEmployer from './view/pages/jobEmployer/JobEmployer.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -23,9 +22,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/job-candidate" element={<JobCandidate />} />
           <Route path="employer" element={<EmployerWrapper />}>
             <Route index element={<JobsEmployer />} />
-            <Route path="candidates" element={<JobCandidate/>} />
-            <Route path="applications" element={<JobApplication />} />
-            <Route path="applications/status" element={<ApplicationStatus />} />
+            <Route path="candidates" element={<JobEmployer jobId={"67c186bb3696a02a49bb0def"}/>} />
             <Route path="chat" element={<Chat />} />
           </Route>
           <Route path="*" element={<div>Not Found</div>} />
