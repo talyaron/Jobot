a6db86dee1b49a76d10519cb76942370878ae300
Elizigi <elizigi876@gmail.com>
2025-03-04 19:34:24 +0200

    little fix on server

--- server/src/Routes/userRoutes.ts
+++ server/src/Routes/userRoutes.ts
@@ -5,7 +5,7 @@ import { getUserProfile } from "../Controllers/user/getUserProfile";
 
 const router = express.Router();
 
-router.get("/matched-jobs/:userId", getMatchedJobs);
+router.get("/matched-jobs/:userId",authMiddleware, getMatchedJobs);
 
 router.get("/profile", getUserProfile);
 
