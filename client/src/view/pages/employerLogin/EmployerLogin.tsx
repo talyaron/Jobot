b6dea7ca44f267b5aecb456f51c919a0642db955
Elizigi <elizigi876@gmail.com>
2025-03-21 11:55:36 +0200

    refactor/style for employer login

--- client/src/view/pages/employerLogin/EmployerLogin.tsx
+++ client/src/view/pages/employerLogin/EmployerLogin.tsx
@@ -1,68 +1,84 @@
-import { EmployerLoginVM } from './EmployerLoginVM'
-import styles from "./employerLogin.module.scss"
+import { EmployerLoginVM } from "./EmployerLoginVM";
+import styles from "./employerLogin.module.scss";
 
 const EmployerLogin = () => {
-    const {handleChange, handleSubmit, isLogin, formData, setIsLogin} = EmployerLoginVM();
+  const { handleChange, handleSubmit, isLogin, formData, setIsLogin } =
+    EmployerLoginVM();
   return (
     <div className={styles.pageWrapper}>
-      <h1>Employer Login</h1>
       <div className={styles.authContainer}>
-      <h2>{isLogin ? "Login" : "Register"}</h2>
-      <form onSubmit={handleSubmit} className={styles.authForm}>
-        {!isLogin && (
-          <>
-            <input
-              type="text"
-              name="userName"
-              placeholder="Username"
-              value={formData.userName}
-              onChange={handleChange}
-              required
-            />
-            <input
-              type="tel"
-              name="phoneNumber"
-              placeholder="Phone Number"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              required
-            />
-          </>
-        )}
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        {!isLogin && (
+      <h1>התחברות מעסיקים</h1>
+        <h2>{isLogin ? "התחברות" : "הרשמה"}</h2>
+        <form onSubmit={handleSubmit} className={styles.authForm}>
+          {!isLogin && (
+            <>
+              <p>שם מלא</p>
+              <input
+                type="text"
+                name="userName"
+                placeholder="...הקלד שם מלא"
+                value={formData.userName}
+                onChange={handleChange}
+                required
+              />
+              <p>טלפון</p>
+
+              <input
+                type="tel"
+                name="phoneNumber"
+                placeholder="...הקלד טלפון"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                required
+              />
+            </>
+          )}
+          <p>אימייל</p>
+
           <input
-            type="password"
-            name="rePassword"
-            placeholder="Confirm Password"
-            value={formData.rePassword}
+            type="email"
+            name="email"
+            placeholder="...הקלד אימייל"
+            value={formData.email}
             onChange={handleChange}
             required
           />
-        )}
-        <button type="submit">{isLogin ? "Login" : "Register"}</button>
-      </form>
-      <button onClick={() => setIsLogin(!isLogin)} className={styles.toggleText}>
-        {isLogin ? "Don't have an account? Register" : "Already have an account? Login"}
-      </button>
-    </div>
-    </div>
-  )
-}
+          <p>סיסמה</p>
 
-export default EmployerLogin
+          <input
+            type="password"
+            name="password"
+            placeholder="...הקלד סיסמה"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+
+          {!isLogin && (
+            <>
+                      <p>סיסמה בשנית</p>
+            <input
+              type="password"
+              name="rePassword"
+              placeholder="...הקלד סיסמה בשנית"
+              value={formData.rePassword}
+              onChange={handleChange}
+              required
+            />
+          </>)}
+          <button type="submit">{isLogin ? "התחברות" : "הרשמה"}</button>
+        </form>
+        <button
+          onClick={() => setIsLogin(!isLogin)}
+          className={styles.toggleText}
+        >
+          {isLogin
+             ? "אין לך חשבון? הירשם"
+             : "כבר יש לך חשבון? התחבר"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EmployerLogin;
