b2f7715a1eac0ae80cba2eba75fb85c5d4390548
silam50 <ysilam50@gmail.com>
2025-03-21 12:31:36 +0200

    btns

---
+++ client/src/view/styles/CheckComp.tsx
@@ -0,0 +1,21 @@
+import './buttons.scss'
+
+const CheckComp = ({}: {
+
+}) => {
+  return (
+    <div className="card">
+      <button className='btn smallBtn'>הגשת מועמדות </button>
+      <button className='btn bigBtn'>חפש משרות </button>
+      <button className='btn roundedBtn'>התחברות </button>
+
+
+
+    </div>
+  );
+};
+
+
+
+
+export default CheckComp
