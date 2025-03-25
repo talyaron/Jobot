86988a680cfe3f79d891b143a00da6de66826e68
Tal Yaron <tal.yaron@gmail.com>
2025-03-25 19:09:43 +0200

    fixed errors

--- server/src/server.ts
+++ server/src/server.ts
@@ -38,6 +38,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/employer/jobs", employerRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/userJob", userJobRoutes);
 
 const dbUrl = process.env.DB_URL;
 const database = 'jobot';
