d173b80a1182befdef526dff8a61b340a810885a
Yosef Ibrahim <yosefib88@gmail.com>
2025-03-28 13:18:04 +0300

    InJobsEmployer: see all candiates

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./view/styles/style.scss";
 
+import DesignCvWizard from './view/pages/designCvWizard/DesignCvWizard.tsx'
 
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
@@ -36,12 +37,13 @@ createRoot(document.getElementById('root')!).render(
             <Route path=":userId" element={<Results />} />
             <Route path="job-application/:user-jobId" element={<JobApplication />} />
             <Route path="chat/:jobId" element={<Chat />} />
+            <Route path="cv" element={<DesignCvWizard />} />
           </Route>
           <Route path="employer/login" element={<EmployerLogin />} />
           <Route path="employer" element={<EmployerWrapper />}>
             <Route index element={<EmployerMain />} />
             <Route path="candidates" element={<JobCandidate />} />
-            <Route path="application/:applicationId" element={<JobApplication />} />
+            <Route path="applications" element={<JobApplication />} />
             <Route path="applications/status" element={<ApplicationStatus />} />
             <Route path="chat/:candidateId" element={<Chat />} />
           </Route>
@@ -49,5 +51,6 @@ createRoot(document.getElementById('root')!).render(
         </Routes>
       </BrowserRouter>
     </Provider>
+
   </StrictMode>,
 )
--- client/src/view/pages/employerWrapper/components/candidates/Candidates.tsx
+++ client/src/view/pages/employerWrapper/components/candidates/Candidates.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { useCandidates } from './CandidatesVM'
 import { Link } from 'react-router'
-
+import CandidateCard from '../../../../components/candidatesCard/CandidatesCard';
 const Candidates = () => {
     const { candidates } = useCandidates();
     console.log(candidates)
     return (
-        <div>
-            <h2>Candidates</h2>
-            <ul>
-                {candidates?.map((candidate:any) => (
-                    <Link to={`application/${candidate.applicationId}`} key={candidate.applicationId}><li >{candidate.candidate.email}</li></Link>
-                ))}
-            </ul>
-        </div>
+      <div>
+      <h2>Candidates</h2>
+      {candidates.map(can => (
+          <CandidateCard
+          email={can.candidate.email}
+          phone={can.candidate.phoneNumber}
+          />
+      ))}
+  </div>
     )
 }
 
