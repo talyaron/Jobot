ca4a6ccc3c330c775c7c5a796cbf7a61b43cd1a5
Tal Yaron <tal.yaron@gmail.com>
2025-03-28 10:21:06 +0300

    candidates

--- server/src/Routes/employerRoutes.ts
+++ server/src/Routes/employerRoutes.ts
@@ -3,6 +3,8 @@ import { createJob } from "../Controllers/Employer/job/createJob";
 import { getAllJobs, getJobsByEmployer } from "../Controllers/Employer/job/fetchJobs";
 import { deleteJob } from "../Controllers/Employer/job/deleteJob";
 import { editJob } from "../Controllers/Employer/job/editJob";
+import { getCandidatesByEmployerId } from "../Controllers/job/getJobs";
+import { userIdMiddleware } from "../utils/authMiddleware";
 
 const router = express.Router();
 
@@ -15,5 +17,6 @@ router.get("/get-jobs-by-employer", getJobsByEmployer);
 router.delete(`/delete/:id`, deleteJob);
 
 router.put(`/edit/:id`, editJob);
+router.get("/get-candidates-by-employer-id", userIdMiddleware, getCandidatesByEmployerId);
 
 export default router;
